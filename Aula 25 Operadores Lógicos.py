00e8258443d62974679136bcdfd6f79a2ebbb8b6
Rafael Nunes <rafaelnunes44@gmail.com>
2020-04-29 14:28:49 -0300

    Blocos de Instrução I

---
+++ Aula 25 Operadores Lógicos.py
@@ -0,0 +1,16 @@
+#==========================================
+#============ www. eXcript.com ============
+#==========================================
+
+"""
+and - e
+or - ou
+not - negação
+is - é
+is not - não é
+in - contido
+not in - não está contido
+
+"""
+print(2 > 4)       #Resposta é False
+print(not (2 > 4)) #Resposta é True
---
+++ Aula 26 Bloco de Instrução I.py
@@ -0,0 +1,17 @@
+#==========================================
+#============ www. eXcript.com ============
+#==========================================
+
+# -*- coding: utf-8 -*-
+
+if True:
+    print("Imprime um texto na tela")
+    print("Imprime um texto na tela")
+    print("Imprime um texto na tela")
+    print("Imprime um texto na tela")
+
+if False:
+    print("Esse texto não irá imprimir")
+    print("Esse texto não irá imprimir")
+    print("Esse texto não irá imprimir")
+    print("Esse texto não irá imprimir")
