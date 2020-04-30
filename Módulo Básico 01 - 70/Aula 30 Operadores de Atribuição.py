0a528e646729325bc606cb12979b4725adcaa9eb
Rafael Nunes <rafaelnunes44@gmail.com>
2020-04-30 16:15:14 -0300

    Criado Diretórios

---
+++ Módulo Básico 01 - 70/Aula 30 Operadores de Atribuição.py
@@ -0,0 +1,27 @@
+#==========================================
+#============ www. eXcript.com ============
+#==========================================
+
+"""
+Atribuição
+
+o valor ao lado direito do operador é atribuído à variável a esquerda do operador.
+
+#Atribuição
+x = y
+
+#Comparação
+x == y
+
+"""
+
+a = 9
+
+x = y = z = a
+print(a)
+print(z)
+print(y)
+print(x)
+
+print(a==z==y==x)
+print(type(a))
