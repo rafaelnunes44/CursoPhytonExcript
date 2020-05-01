22acadfee844700f7fe6d18fdacab398f306ba0a
Rafael Nunes <rafaelnunes44@gmail.com>
2020-05-01 17:41:58 -0300

    Operadores de Atribuição Compostos

---
+++ Módulo Básico 01 - 70/Aula 31 Atribuição Múltipla.py
@@ -0,0 +1,42 @@
+#==========================================
+#============ www. eXcript.com ============
+#==========================================
+
+"""
+Atibuição múltipla é a capacidade de atribuir valores
+diferentes a variáveis distintas numa mesma instrução.
+
+"""
+"""
+a, b, c = 5, 7, 9
+s1, s2 = "curso", "phyton"
+
+print(a)
+print(b)
+print(c)
+print(s1)
+print(s2)
+"""
+
+"""
+#Invertendo os valores das variáveis
+a = 10
+b = 5
+
+a, b = b, a
+print(a, b)
+"""
+
+"""
+x, y, z = 2, 4, 8
+x, y, z = x*2, x+y+z, x*y*z
+print(x)
+print(y)
+print(z)
+
+"""
+
+#Atribuindo String
+
+nome, sobrenome = "Rafael", "Nunes"
+print(nome, sobrenome)
---
+++ Módulo Básico 01 - 70/Aula 32 Operadores de Atribuição Compostos.py
@@ -0,0 +1,49 @@
+#==========================================
+#============ www. eXcript.com ============
+#==========================================
+
+#Primeira Operacao
+"""
+x = 9
+y = 3
+x += y
+print(x) #(x += y) = 12
+"""
+
+#Segunda Operacao
+"""
+x = 9
+y = 3
+x -= y
+print(x) #(x -= y) = 6
+"""
+
+#Terceira Operacao
+"""
+x = 9
+y = 3
+x *= y
+print(x) #(x *= y) = 27
+"""
+
+#Quarta Operacao
+"""
+x = 9
+y = 3
+x /= y
+print(x) #(x /= y) = 3
+"""
+
+#Quinta Operacao
+"""
+x = 9
+y = 3
+x %= y
+print(x) #(x %= y) = 0
+"""
+
+
+
+
+
+
